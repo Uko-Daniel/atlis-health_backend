8d68727a547dd1310fd9b01a92ed3d0c3fdf172b
Uko-Daniel <linusdaniel17@gmail.com>
2026-05-23 16:29:54 +0100

    May 23rd

--- src/routes/patientRoutes.ts
+++ src/routes/patientRoutes.ts
@@ -2,10 +2,10 @@ import { type FastifyInstance } from 'fastify';
 import { patientController } from '../controllers/patientController';
 
 export async function patientRoutes(fastify: FastifyInstance) {
-    fastify.post('/patients', patientController.registerPatient);
+    fastify.post('/patients', patientController.createPatient);
     fastify.get('/patients/search', patientController.searchPatients);
-    fastify.get('/patients', patientController.fetchAllPatients);
-    fastify.get('/patients/:id', patientController.fetchPatientById);
+    fastify.get('/patients', patientController.getAllPatients);
+    fastify.get('/patients/:id', patientController.getPatientById);
     fastify.put('/patients/:id', patientController.updatePatient);
     fastify.delete('/patients/:id', patientController.deletePatient);
 }
